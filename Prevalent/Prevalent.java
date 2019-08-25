ab7110a291b2f1495d85c573cf118a1311ade481
Anchinga <48989672+Anchinga@users.noreply.github.com>
2019-08-25 23:06:28 +0300

    Add files via upload
    This is the initial commit

---
+++ Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.ksoko.Prevalent;
+
+import com.ksoko.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
